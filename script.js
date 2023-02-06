17938c01d10720c0b9cfc7ab0e9d684cea6ff22a
Ale <ale.surdu29@gmail.com>
2023-02-06 21:51:44 +0100

    Add erase button and functionality

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-
-
 function createGrid() {
     const gridContainer = document.querySelector('.grid');
     // const gridSize = prompt("enter grid size");
-    for(let i = 0; i < 23; i++){
+    for(let i = 0; i < 40; i++){
         const gridRow = document.createElement('div');
         gridRow.classList.add('grid-row');
-        for(let j = 0; j < 23; j++) {
+        for(let j = 0; j < 40; j++) {
             const gridCol = document.createElement('div');
             gridCol.classList.add('grid-col');
             gridRow.appendChild(gridCol);
@@ -19,7 +17,7 @@ function createGrid() {
 function draw(){
     const squares = document.querySelectorAll('.grid-col');
     squares.forEach(square => square.addEventListener('mousemove', (e) => {
-            if(e.buttons == 1) { //if the mouse button is down
+            if(e.buttons == 1) { //checks if the mouse button is down
                 square.style.cssText = 'background-color: black';
             }
     }));
@@ -33,7 +31,28 @@ function clear(){
     });
 }
 
+function erase(){
+    const squares = document.querySelectorAll('.grid-col');
+    const eraseButton = document.querySelector('.eraser');
+    eraseButton.addEventListener('click', () => { 
+        eraseButton.classList.toggle('.erase');
+        if(eraseButton.classList.contains('.erase')) {
+            eraseButton.style.backgroundColor = 'blue';
+            squares.forEach(square => square.addEventListener('mousemove', (e) => {
+            if(e.buttons == 1) { //checks if the mouse button is down
+                square.style.cssText = 'background-color: white';
+                }
+        }));
+        } else {
+            eraseButton.style.backgroundColor = 'gray';
+            draw();
+
+        }
+    });
+}
+
 
 createGrid();
 draw();
 clear();
+erase();
