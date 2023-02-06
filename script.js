042f9f90059114618cf39b30b0feed54a916f77b
Ale <ale.surdu29@gmail.com>
2023-02-06 16:35:33 +0100

    Add regular drawing function

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
+const squares = document.querySelectorAll('.grid-col');
+
 function createGrid() {
     const gridContainer = document.querySelector('.grid');
-    const gridSize = prompt("enter grid size");
-    for(let i = 0; i < gridSize; i++){
+    // const gridSize = prompt("enter grid size");
+    for(let i = 0; i < 23; i++){
         const gridRow = document.createElement('div');
-        gridRow.style.cssText = 'display: flex; flex: 1';
-        for(let j = 0; j < gridSize; j++) {
+        gridRow.classList.add('grid-row');
+        for(let j = 0; j < 23; j++) {
             const gridCol = document.createElement('div');
-            gridCol.style.cssText = 'display: flex; flex: 1; background-color: salmon; text-align: center; margin: 1px';
+            gridCol.classList.add('grid-col');
             gridRow.appendChild(gridCol);
-            gridContainer.appendChild(gridRow);
+            
+    }
+    gridContainer.appendChild(gridRow);
     }
 }
+
+function draw(){
+    squares.forEach(square => square.addEventListener('mousemove', (e) => {
+            if(e.buttons == 1) { //if the mouse button is down
+                square.style.cssText = 'background-color: black';
+            }
+    }));
+}
+
+function clear(){
+    squares.forEach(square => square.style.cssText = 'background-color: white');
 }
 
 
 createGrid();
+draw();
