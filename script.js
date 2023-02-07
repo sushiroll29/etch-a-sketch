5f493f0f3d6478306ac6afd2b9f8c6ac5362df9c
Ale <ale.surdu29@gmail.com>
2023-02-07 10:45:32 +0100

    Add change grid size functionality

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+let gridSize;
+const eraseButton = document.querySelector('.eraser');
+const rainbowButton = document.querySelector('.rainbow');
+const clearButton = document.querySelector(".clear");
+const gridContainer = document.querySelector('.grid');
 
-function createGrid() {
-    const gridContainer = document.querySelector('.grid');
-    // const gridSize = prompt("enter grid size");
-    for(let i = 0; i < 40; i++){
+//functions
+function createGrid(gridSize) {
+    for(let i = 0; i < gridSize; i++){
         const gridRow = document.createElement('div');
         gridRow.classList.add('grid-row');
-        for(let j = 0; j < 40; j++) {
+        for(let j = 0; j < gridSize; j++) {
             const gridCol = document.createElement('div');
             gridCol.classList.add('grid-col');
             gridRow.appendChild(gridCol);
@@ -13,10 +17,14 @@ function createGrid() {
     }
     gridContainer.appendChild(gridRow);
     }
+draw();
+clear();
+erase();
+rainbow();
 }
 
 function setSquareColor(color) {
-    const squares = document.querySelectorAll('.grid-col');
+    const squares = gridContainer.querySelectorAll('.grid-col');
     squares.forEach(square => square.addEventListener('mousemove', (e) => {
             if(e.buttons == 1) { //checks if the mouse button is down
                 square.style.backgroundColor = color;
@@ -29,7 +37,6 @@ function draw(){
 }
 
 function clear(){
-    const clearButton = document.querySelector(".clear");
     const squares = document.querySelectorAll('.grid-col');
     clearButton.addEventListener('click', () => {
         squares.forEach(square => square.style.cssText = 'background-color: white');
@@ -37,10 +44,25 @@ function clear(){
 }
 
 function erase(){
-    const eraseButton = document.querySelector('.eraser');
-    const rainbowButton = document.querySelector('.rainbow');
-    eraseButton.addEventListener('click', () => { 
-        eraseButton.classList.toggle('.erase');
+    eraseButton.addEventListener('click', eraseHandler);
+}
+
+function rainbow(){
+    rainbowButton.addEventListener('click', rainbowHandler);
+}
+
+function changeSize() {
+    const changeButton = document.querySelector(".size");
+    changeButton.addEventListener('mouseup', changeSizeHandler);
+}
+
+//function calls
+createGrid(30);
+changeSize();
+
+//handlers
+function eraseHandler(){
+    eraseButton.classList.toggle('.erase');
         if(eraseButton.classList.contains('.erase')) {
             rainbowButton.disabled = true;
             eraseButton.style.backgroundColor = '#DEDEDE';
@@ -50,15 +72,11 @@ function erase(){
             rainbowButton.disabled = false;
             draw();
         }
-    });
 }
 
-function rainbow(){
+function rainbowHandler() {
     const squares = document.querySelectorAll('.grid-col');
-    const rainbowButton = document.querySelector('.rainbow');
-    const eraseButton = document.querySelector('.eraser');
-    rainbowButton.addEventListener('click', () => {
-        rainbowButton.classList.toggle('.color');
+    rainbowButton.classList.toggle('.color');
         if(rainbowButton.classList.contains('.color')) {
             eraseButton.disabled = true;
             rainbowButton.style.backgroundColor = '#DEDEDE';
@@ -68,19 +86,23 @@ function rainbow(){
                     square.style.backgroundColor = '#' + randomColor;
                     }
             }));
-            } else {
-                rainbowButton.style.backgroundColor = 'white';
-                eraseButton.disabled = false;
-                draw();
-    
-            }
+        } else {
+            rainbowButton.style.backgroundColor = 'white';
+            eraseButton.disabled = false;
+            draw();
         }
-    );
 }
 
+function changeSizeHandler(){
+    let size = prompt("What size do you want the grid to be? (max. 100)");
+    let cols = document.querySelectorAll('.grid-col');
+    let rows = document.querySelectorAll('.grid-row');
+    
+    rainbowButton.removeEventListener('click', rainbowHandler);
+    eraseButton.removeEventListener('click', eraseHandler);
 
-createGrid();
-draw();
-clear();
-erase();
-rainbow();
+    cols.forEach(col => col.remove());
+    rows.forEach(row => row.remove());
+
+    createGrid(size); 
+}
